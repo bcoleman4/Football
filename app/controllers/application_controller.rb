3bd9129c7e38e0b789ab11c3c3218a870bd88c15
bcoleman4 <bcoleman4@hotmail.co.uk>
2014-11-28 00:14:27 +0000

    Gemfile update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     def hello
-    render text: "hello, world!"
-    render text: "The classic bit of text"
+    render text: "hello, world!" " The classic bit of text"
   end
 end
